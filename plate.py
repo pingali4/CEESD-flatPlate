3303d37a63479e7e17b260e5ce099486c46536f5
Pingali <pingali4@cc-login1.campuscluster.illinois.edu>
2021-07-23 10:31:18 -0500

    Plate updated

--- plate.py
+++ plate.py
@@ -41,7 +41,7 @@ import yaml
 from pytools.obj_array import ( 
     obj_array_vectorize, make_obj_array
 )
-import pickle
+#import pickle
 
 from grudge.dof_desc import DTAG_BOUNDARY
 from meshmode.array_context import PyOpenCLArrayContext
@@ -49,7 +49,7 @@ from meshmode.dof_array import thaw, flatten, unflatten
 from meshmode.mesh import BTAG_ALL, BTAG_NONE  # noqa
 from grudge.eager import EagerDGDiscretization
 from grudge.shortcuts import make_visualizer
-from grudge.op import nodal_max
+from grudge.op import nodal_max, nodal_min
 
 from mirgecom.profiling import PyOpenCLProfilingArrayContext
 
@@ -60,8 +60,9 @@ from mirgecom.fluid import (
     make_conserved
 )
 
-from mirgecom.inviscid import get_inviscid_cfl
+#from mirgecom.inviscid import get_inviscid_cfl
 from mirgecom.simutil import (
+    generate_and_distribute_mesh,
     get_sim_timestep,
     check_step,
     generate_and_distribute_mesh,
@@ -71,7 +72,6 @@ from mirgecom.restart import (
     write_restart_file
 )
 
-from mirgecom.restart import(write_restart_file)
 
 from mirgecom.io import make_init_message
 from mirgecom.mpi import mpi_entry_point
@@ -95,29 +95,29 @@ from mirgecom.initializers import (
 from mirgecom.eos import IdealSingleGas
 from mirgecom.transport import SimpleTransport
 
-from logpyle import IntervalTimer, LogQuantity
+from logpyle import IntervalTimer, set_dt
 
 from mirgecom.euler import extract_vars_for_logging, units_for_logging
 from mirgecom.logging_quantities import (initialize_logmgr,
     logmgr_add_many_discretization_quantities, logmgr_add_cl_device_info,
-    logmgr_set_time, LogUserQuantity)
+    logmgr_set_time, LogUserQuantity, set_sim_state)
 logger = logging.getLogger(__name__)
 
 
+class MyRuntimeError(RuntimeError):
+    """Simple exception to kill the simulation."""
+
+    pass
 
 
 @mpi_entry_point
-def main(ctx_factory=cl.create_some_context, casename = "plate", user_input_file = None,  
-         snapshot_pattern="{casename}-{step:06d}-{rank:04d}.pkl", restart_step=None, 
-         restart_name = None, use_profiling=False, use_logmgr=False, use_lazy_eval=False):
+def main(ctx_factory=cl.create_some_context, casename = "plate", user_input_file = None, restart_file=None,use_profiling=False, use_logmgr=False, use_lazy_eval=False):
 
     from mpi4py import MPI
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     nparts = comm.Get_size()
 
-    if restart_name is None:
-      restart_name=casename
 
     logmgr = initialize_logmgr(use_logmgr, filename=(f"{casename}.sqlite"),
         mode="wo", mpi_comm=comm)
@@ -134,7 +134,8 @@ def main(ctx_factory=cl.create_some_context, casename = "plate", user_input_file
     else:
         queue = cl.CommandQueue(cl_ctx)
         if use_lazy_eval:
-            actx = PytatoArrayContext(queue)
+            from meshmode.array_context import PytatoPyOpenCLArrayContext
+            actx = PytatoPyOpenCLArrayContext(queue)
         else:
             actx = PyOpenCLArrayContext(queue,
                 allocator=cl_tools.MemoryPool(cl_tools.ImmediateAllocator(queue)))
@@ -199,21 +200,21 @@ def main(ctx_factory=cl.create_some_context, casename = "plate", user_input_file
            
         return generate_mesh
 
-    print(f"restart_step {restart_step}")
-    if restart_step is None:
-        local_mesh, global_nelements = generate_and_distribute_mesh(comm, get_mesh())
-        local_nelements = local_mesh.nelements
+   # print(f"restart_step {restart_step}")
+    #if restart_step is None:
+     #   local_mesh, global_nelements = generate_and_distribute_mesh(comm, get_mesh())
+      #  local_nelements = local_mesh.nelements
 
-    else:  # Restart
-        print(f"restarting from {restart_step}")
-        with open(snapshot_pattern.format(casename=casename, step=restart_step, rank=rank), "rb") as f:
-            restart_data = pickle.load(f)
+    #else:  # Restart
+     #   print(f"restarting from {restart_step}")
+      #  with open(snapshot_pattern.format(casename=casename, step=restart_step, rank=rank), "rb") as f:
+       #     restart_data = pickle.load(f)
 
-        local_mesh = restart_data["local_mesh"]
-        local_nelements = local_mesh.nelements
-        global_nelements = restart_data["global_nelements"]
+        #local_mesh = restart_data["local_mesh"]
+        #local_nelements = local_mesh.nelements
+        #global_nelements = restart_data["global_nelements"]
 
-        assert comm.Get_size() == restart_data["num_parts"]
+        #assert comm.Get_size() == restart_data["num_parts"]
 
 
     #nviz = 500
@@ -291,13 +292,6 @@ def main(ctx_factory=cl.create_some_context, casename = "plate", user_input_file
 
     vel_inflow = np.zeros(shape=(dim,))
     vel_outflow = np.zeros(shape=(dim,))
-
-    if rank == 0:
-        logging.info("Making discretization")
-    discr = EagerDGDiscretization(
-        actx, local_mesh, order=order, mpi_communicator=comm
-    )
-    nodes = thaw(actx, discr.nodes())
 
     # working gas: air #
     #   gamma = 1.289
@@ -468,12 +462,43 @@ def main(ctx_factory=cl.create_some_context, casename = "plate", user_input_file
                   DTAG_BOUNDARY("PlateUpstream"): bottom_symmetry,
                   DTAG_BOUNDARY("Plate"): plate}
 
-    if restart_step is None:
+    viz_path = "viz_data/"
+    restart_path = "restart_data/"
+    snapshot_pattern = restart_path+"/{cname}-{step:06d}-{rank:04d}.pkl"
+    vizname = viz_path + casename
+   
+    restart_step = None
+    if restart_file is None:
+        local_mesh, global_nelements = generate_and_distribute_mesh(
+            comm,
+            get_mesh()
+        )
+        local_nelements = local_mesh.nelements
+
+    else:  # Restart
+        from mirgecom.restart import read_restart_data
+        restart_data = read_restart_data(actx, restart_file)
+        restart_step = restart_data["step"]
+        local_mesh = restart_data["local_mesh"]
+        local_nelements = local_mesh.nelements
+        global_nelements = restart_data["global_nelements"]
+        restart_order = int(restart_data["order"])
+
+        assert comm.Get_size() == restart_data["num_parts"]
+
+    if rank == 0:
+        logging.info("Making discretization")
+    discr = EagerDGDiscretization(
+        actx, local_mesh, order=order, mpi_communicator=comm
+    )
+    nodes = thaw(actx, discr.nodes())
+
+    if restart_file is None: 
         if rank == 0:
             logging.info("Initializing soln.")
         # for Discontinuity initial conditions
         #current_state = bulk_init
-        current_state = bulk_init(x_vec=nodes, eos=eos, t=0)
+        current_state = bulk_init(x_vec=nodes, eos=eos, t=0.0)
     else:
         current_t = restart_data["t"]
         current_step = restart_step
@@ -518,7 +543,7 @@ def main(ctx_factory=cl.create_some_context, casename = "plate", user_input_file
         vis_timer = IntervalTimer("t_vis", "Time spent visualizing")
         logmgr.add_quantity(vis_timer)
 
-    visualizer = make_visualizer(discr, order )
+    visualizer = make_visualizer(discr)
     initname = "plate"
     eosname = eos.__class__.__name__
     init_message = make_init_message(dim=dim, order=order,
@@ -533,121 +558,151 @@ def main(ctx_factory=cl.create_some_context, casename = "plate", user_input_file
     if rank == 0:
         logger.info(init_message)
 
-    get_timestep = partial(get_sim_timestep, discr=discr, t=current_t,
-                           dt=current_dt, cfl=current_cfl, eos=eos,
-                           t_final=t_final, constant_cfl=constant_cfl)
+    #get_timestep = partial(get_sim_timestep, discr=discr, t=current_t,
+                   #        dt=current_dt, cfl=current_cfl, eos=eos,
+                    #       t_final=t_final, constant_cfl=constant_cfl)
 
     def my_rhs(t, state):
         # check for some troublesome output types
 
         return (ns_operator(discr, cv= state, t=t,boundaries=boundaries, eos=eos))
-
-    def my_pre_step(step, t, dt, state):
-        dt = current_dt
-        if logmgr:
-            logmgr.tick_before()
-
-        my_checkpoint(step, t, dt, state)
-        return state, dt
-
-    def my_post_step(step, t, dt, state):
-        dt = current_dt
-        if logmgr:
-            # set_dt(logmgr, dt)
-            # set_sim_state(logmgr, dim, state, eos)
-            logmgr.tick_after()
-        return state, dt
-
-    def my_checkpoint(step, t, dt, state, force=False):
-
-        do_health = force or check_step(step, nhealth) and step > 0
-        do_viz = force or check_step(step, nviz)
-        do_restart = force or check_step(step, nrestart)
-        do_status = force or check_step(step, nstatus)
-
-        if do_viz or do_health:
+    def my_write_viz(step, t, dt, state, dv=None,  ts_field=None):
+        if dv is None:
             dv = eos.dependent_vars(state)
+        if ts_field is None:
+            ts_field, cfl, dt = my_get_timestep(t, dt, state)
+            #t except MyRuntimeError:
+            #if rank == 0:
+             #   logger.info("Errors detected; attempting graceful exit.")
+            #my_write_viz(step=step, t=t, dt=dt, state=state)
+            #my_write_restart(step=step, t=t, state=state)
+            #raises_field, cfl, dt = my_get_timestep(t, dt, state)
 
-        errors = False
-        if do_health:
-            health_message = ""
-            if check_naninf_local(discr, "vol", dv.pressure):
-                errors = True
-                health_message += "Invalid pressure data found.\n"
-            elif check_range_local(discr,
-                                   "vol",
-                                   dv.pressure,
-                                   min_value=1,
-                                   max_value=2.0e6):
-                errors = True
-                health_message += "Pressure data failed health check.\n"
+        viz_fields = [("cv", state),
+                      ("dv", dv),
+                      ("dt" if constant_cfl else "cfl", ts_field)]
+        write_visfile(discr, viz_fields, visualizer, vizname=vizname,
+                      step=step, t=t, overwrite=True)
 
-        errors = comm.allreduce(errors, MPI.LOR)
-        if errors:
-            if rank == 0:
-                logger.info("Fluid solution failed health check.")
-            if health_message:
-                logger.info(f"{rank=}:  {health_message}")
-
-        #if check_step(step, nrestart) and step != restart_step and not errors:
-        if do_restart or errors:
-            filename = restart_path + snapshot_pattern.format(
-                step=step, rank=rank, casename=casename)
-            restart_dictionary = {
+    def my_write_restart(step, t, state):
+        rst_fname = snapshot_pattern.format(cname=casename, step=step, rank=rank)
+        if rst_fname != restart_file:
+            rst_data = {
                 "local_mesh": local_mesh,
-                "order": order,
                 "state": state,
                 "t": t,
                 "step": step,
+                "order": order,
                 "global_nelements": global_nelements,
                 "num_parts": nparts
             }
-            write_restart_file(actx, restart_dictionary, filename, comm)
+            write_restart_file(actx, rst_data, rst_fname, comm)
 
-        if do_status or do_viz or errors:
-            local_cfl = get_inviscid_cfl(discr, eos=eos, dt=dt, cv=state)
-            max_cfl = nodal_max(discr, "vol", local_cfl)
-            log_cfl.set_quantity(max_cfl)
+    def my_health_check(dv):
+        health_error = False
+        if check_naninf_local(discr, "vol", dv.pressure):
+            health_error = True
+            logger.info(f"{rank=}: NANs/Infs in pressure data.")
 
-        #if ((check_step(step, nviz) and step != restart_step) or errors):
-        if do_viz or errors:
-            viz_fields = [("cv", state), ("dv", eos.dependent_vars(state)),
-                        ("cfl", local_cfl)]
-            write_visfile(discr,
-                          viz_fields,
-                          visualizer,
-                          vizname=viz_path + casename,
-                          step=step,
-                          t=t,
-                          overwrite=True,
-                          vis_timer=vis_timer)
+        if check_range_local(discr, "vol", dv.pressure, 1e-1, 2e6):
+            health_error = True
+            logger.info(f"{rank=}: Pressure range violation.")
 
-        if errors:
-            raise RuntimeError("Error detected by user checkpoint, exiting.")
+        return health_error
 
-        return dt
+    def my_get_timestep(t, dt, state):
+        t_remaining = max(0, t_final - t)
+        if constant_cfl:
+            from mirgecom.viscous import get_viscous_timestep
+            ts_field = current_cfl * get_viscous_timestep(discr, eos=eos, cv=state)
+            from grudge.op import nodal_min
+            dt = nodal_min(discr, "vol", ts_field)
+            cfl = current_cfl
+        else:
+            from mirgecom.viscous import get_viscous_cfl
+            ts_field = get_viscous_cfl(discr, eos=eos, dt=dt, cv=state)
+            from grudge.op import nodal_max
+            cfl = nodal_max(discr, "vol", ts_field)
+
+        return ts_field, cfl, min(t_remaining, dt)
+
+    def my_pre_step(step, t, dt, state):
+       try:
+          dv = None
+       	  dt = current_dt
+          if logmgr:
+              	logmgr.tick_before()
+          #dt = get_sim_timestep(discr, state, t, dt, current_cfl, eos,
+           #                       t_final, constant_cfl)
+
+          my_checkpoint(step, t, dt, state)
+       except MyRuntimeError:
+          if rank == 0:
+                logger.info("Errors detected; attempting graceful exit.")
+          my_write_viz(step=step, t=t, dt=dt, state=state)
+          my_write_restart(step=step, t=t, state=state)
+          raise
+       return state, dt
+
+    def my_post_step(step, t, dt, state):
+       # dt = current_dt
+       if logmgr:
+            set_dt(logmgr, dt)
+            set_sim_state(logmgr, dim, state, eos)
+            logmgr.tick_after()
+       return state, dt
+
+    def my_checkpoint(step, t, dt, state, force=False):
+        dv = None
+        do_health = force or check_step(step, nhealth) and step > 0
+        do_viz = force or check_step(step, nviz)
+        do_restart = force or check_step(step, nrestart)
+
+        if  do_health:
+            dv = eos.dependent_vars(state)
+            from mirgecom.simutil import allsync
+            health_errors = allsync(my_health_check(dv), comm,
+                                        op=MPI.LOR)
+            if health_errors:
+                    if rank == 0:
+                        logger.info("Fluid solution failed health check.")
+                    raise MyRuntimeError("Failed simulation health check.")
+
+        if do_restart:
+            my_write_restart(step=step,t=t,state=state)
+
+        
+        if do_viz:
+            if dv is None:
+                    dv = eos.dependent_vars(state)
+            my_write_viz(step=step, t=t, dt=dt, state=state, dv=dv)
 
 
     if rank == 0:
         logging.info("Stepping.")
+    current_dt = get_sim_timestep(discr, current_state, current_t, current_dt,
+                                  current_cfl, eos, t_final, constant_cfl)
 
     (current_step, current_t, current_state) = \
         advance_state(rhs=my_rhs, timestepper=timestepper,
                       pre_step_callback=my_pre_step,
-                      post_step_callback=my_post_step,
-                      get_timestep=get_timestep, state=current_state,
-                      t_final=t_final, t=current_t, istep=current_step,
-                      logmgr=logmgr,eos=eos,dim=dim)
+                      post_step_callback=my_post_step, state=current_state,
+                      t_final=t_final, t=current_t, istep=current_step)
 
     if rank == 0:
         logger.info("Checkpointing final state ...")
+    final_dv = eos.dependent_vars(current_state)
+    final_dt = get_sim_timestep(discr, current_state, current_t, current_dt,
+                                current_cfl, eos, t_final, constant_cfl)
+    my_write_viz(step=current_step, t=current_t, dt=final_dt, state=current_state,
+                 dv=final_dv)
+    my_write_restart(step=current_step, t=current_t, state=current_state)
+    #my_checkpoint(current_step, t=current_t,
+     #             dt=(current_t - checkpoint_t),
+      #            state=current_state, force = True)
 
-    my_checkpoint(current_step, t=current_t,
-                  dt=(current_t - checkpoint_t),
-                  state=current_state, force = True)
-
-    if current_t - t_final < 0:
-        raise ValueError("Simulation exited abnormally")
+    #if current_t - t_final < 0:
+        #raise ValueError("Simulation exited abnormally")
 
     if logmgr:
         logmgr.close()
@@ -693,14 +748,18 @@ if __name__ == "__main__":
     else:
         print(f"Default casename {casename}")
 
-    snapshot_pattern="{casename}-{step:06d}-{rank:04d}.pkl"
-    restart_step=None
-    if(args.restart_file):
-        print(f"Restarting from file {args.restart_file}")
-        restart_step = int(args.restart_file.split('-')[1])
+    #snapshot_pattern="{casename}-{step:06d}-{rank:04d}.pkl"
+    #restart_step=None
+   # if(args.restart_file):
+        #print(f"Restarting from file {args.restart_file}")
+        #restart_step = int(args.restart_file.split('-')[1])
         #print(f"step {restart_step}")
     #print(f"step {restart_step}")
-    
+    restart_file = None
+    if args.restart_file:
+        restart_file = (args.restart_file).replace("'", "")
+        print(f"Restarting from file: {restart_file}")
+
     input_file=None
     if(args.input_file):
         input_file = (args.input_file).replace("'","")
@@ -708,7 +767,12 @@ if __name__ == "__main__":
     else:
         print("No user input file, using default values")
     print(f"Running {sys.argv[0]}\n")
-    main(restart_step=restart_step, snapshot_pattern=snapshot_pattern,
-         use_profiling=args.profile, use_lazy_eval=args.lazy, use_logmgr=args.log)
+    #main(restart_step=restart_step, snapshot_pattern=snapshot_pattern,
+     #    use_profiling=args.profile, use_lazy_eval=args.lazy, use_logmgr=args.log)
+    main(restart_file=restart_file,
+         user_input_file=input_file,
+         use_profiling=args.profile,
+         use_lazy_eval=args.lazy,
+         use_logmgr=args.log)
 
 # vim: foldmethod=marker
